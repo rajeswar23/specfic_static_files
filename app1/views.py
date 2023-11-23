ba20820e9999bbc8374ccb0b9ca8f52e239a8603
rajeswar23 <chembetisudheer23@gmail.com>
2023-11-23 22:41:34 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rajeswari(request):
+    return render(request,'rajeswari.html')
+
+def mowa(request):
+    return render(request,'mowa.html')
